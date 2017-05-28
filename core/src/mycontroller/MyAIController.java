df7bb39dbec87351103367920bccea6d6215ba3d
kitfoster <katya.foster.96@gmail.com>
2017-05-28 14:48:36 +1000

    action

--- core/src/mycontroller/MyAIController.java
+++ core/src/mycontroller/MyAIController.java
@@ -1,11 +1,13 @@
 package mycontroller;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import controller.CarController;
 import map.MapAnalyser;
 import map.PotentialRoom;
 import pathplan.Action;
+import pathplan.Action.Move;
 import pathplan.PathPlanner;
 import pathplan.ReRouter;
 import pathplan.Route;
@@ -15,8 +17,8 @@ import world.Car;
 
 public class MyAIController extends CarController{
 	
-	public enum State {START, EXPLORE, REROUTING, SOLVING};
-	public enum Direction {NORTH, SOUTH, EAST, WEST};
+	public static enum State {START, EXPLORE, REROUTING, SOLVING};
+	public static enum Direction {NORTH, SOUTH, EAST, WEST};
 	
 	private MapAnalyser map = new MapAnalyser();
 	private PathPlanner pathPlanner;
@@ -25,11 +27,14 @@ public class MyAIController extends CarController{
 	private ReRouter reRouter;
 	private Route currentRoute;
 	private Direction direction;
-	private ArrayList<Action> actionsToExcecute = new ArrayList<Action>();
+	private LinkedList<Action> actionsToExcecute = new LinkedList<Action>();
+	private Action nextAction;
+	private Car car;
 	
 
 	public MyAIController(Car car) {
 		super(car);
+		this.car = car;
 	}
 	
 	@Override
@@ -51,8 +56,29 @@ public class MyAIController extends CarController{
 		this.destination = destination;
 	}
 	
-	private void excecuteNextAction(ArrayList<Action> actions) {
-		
+	private void excecuteNextAction(LinkedList<Action> actions) {
+		nextAction = actionsToExcecute.getFirst();
+		if (nextAction.getMove() == Move.FORWARD) {
+			// move forward
+			this.car.applyForwardAcceleration();
+		}
+		if (nextAction.getMove() == Move.REVERSE) {
+			// move backwards
+			this.car.applyReverseAcceleration();
+		}
+		if (nextAction.getMove() == Move.LEFT_TURN) {
+			// turn left
+			this.car.turnLeft(delta);
+		}
+		if (nextAction.getMove() == Move.RIGHT_TURN) {
+			// turn right
+		}
+		if (nextAction.getMove() == Move.LEFT_3_PT_TURN) {
+			// left 3 pt turn
+		}
+		if (nextAction.getMove() == Move.RIGHT_3_PT_TURN) {
+			// right 3 pt turn
+		}
 	}
 	
 	private void setPathPlanner(PathPlanner pp) {
